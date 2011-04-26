fb2284d6b4c1080c8325d71e2dd2b1f72f7a5688
Cyril Jose <cyril_jose@ymail.com>
2011-04-26 13:17:20 -0400

    Updated: Added tests to check accession, definition and sequence but for one entry

--- lib/fasta_parser.rb
+++ lib/fasta_parser.rb
@@ -1,15 +1,17 @@
 class FastaParser
   VERSION = "0.0.1"
   
-  attr_reader :input_file
+  attr_reader :input_file, :fasta_is_open
   
   def initialize input_file
     @input_file = input_file
+    @fasta_is_open = fasta_is_open
   end
   
   def check_arg
     unless ARGV.length == 1
       puts "Usage: <ruby> <file.rb> <fasta_file>"
+      exit
       return false
     end
       return true
@@ -18,33 +20,40 @@ class FastaParser
   def check_if_fasta
     unless @input_file.downcase[-6..-1] == ".fasta"
       puts "Usage: File needs '.fasta' extension!"
+      exit
       return false
     end
       return true
   end
   
   def check_sym
-    f = File.open(@input_file, "r")
-    first_sym = f.each_char.to_a
+    @fasta_is_open = File.open(@input_file, "r")
+    first_sym = @fasta_is_open.each_char.to_a
     unless first_sym[0] == ">"
       puts "Usage: Not a fasta file!"
+      exit
       return false
     end
       return true
-    f.close
   end
   
   def get_accession
-    f = File.open(@input_file, "r")
-    
+    @fasta_is_open = File.open(@input_file, "r")
+    array = @fasta_is_open.to_a
+    accession = array[0].split(/[|]/)[3]
+    return true
   end
   
   def get_definition
-  
+    @fasta_is_open = File.open(@input_file, "r")
+    array = @fasta_is_open.to_a
+    d = array[0].split(/[|]/)[4]
+    definition = d.strip
+    return true
   end
   
   def get_sequence
-  
+    
   end
   
 end
@@ -53,3 +62,5 @@ end
 #p.check_arg
 #p.check_if_fasta
 #p.check_sym
+#p.get_accession
+#p.get_definition
--- test/test_case_file.rb
+++ test/test_case_file.rb
@@ -19,8 +19,40 @@ class TestFasta < Test::Unit::TestCase
     assert_equal(">", first_sym[0])
   end
   
+  # checks accession for first entry
   def test_accession
+    p = FastaParser.new("test/test.fasta")
+    assert(p.get_accession == true)
+  
+    f = File.open("test/test.fasta","r")
+    array = f.to_a
+    accession = array[0].split(/[|]/)[3]
+    assert_equal("NM_2005745.3Acc1", accession)
+  end
+  
+  # checks definition for first entry
+  def test_definition
+    p = FastaParser.new("test/test.fasta")
+    assert(p.get_definition == true) # dummmy test
+    
+    f = File.open("test/test.fasta","r")
+    array = f.to_a
+    d = array[0].split(/[|]/)[4]
+    definition = d.strip
+    assert_equal("Def1 zgc:65895 (zgc:65895), mRNA", definition)
+  end
+  
+  # checks sequence match (NOTE: only one entry in file)
+  def test_sequence
     f = File.open("test/test_entry.fasta","r")
+    array = f.to_a
+    seq_with_newline = ''
+    sequence = ''
+    array.drop(1).each do |element|
+      seq_with_newline += element
+      sequence = seq_with_newline.delete("\n")
+    end
+    assert_equal("AGCTCGGGGGCTCTAGCGATTTAAGGAGCGATGCGATCGAGCTGACCGTCGCGAAGGAGCGGTGAGGGAGAGGAGAGAGGA", sequence)
   end
   
 end
