090f36d6ba0fc29f72fa1b36727eced29e0bd175
Cyril Jose <cyril_jose@ymail.com>
2011-05-03 11:45:03 -0400

    added read_next_entry method

--- lib/fasta_parser.rb
+++ lib/fasta_parser.rb
@@ -1,25 +1,46 @@
+
 class FastaParser
   VERSION = "0.0.1"
-  
-  attr_reader :input_file, :fasta_is_open
-  
+  class Entry
+    #
+  end
+
+  attr_reader :input_file
+
   def initialize input_file
-    @input_file = input_file
-    @fasta_is_open = fasta_is_open
+    @input_file = File.open(input_file, "r")
+    @index_position = index_fasta_file_headers()
   end
-  
-  def check_arg
-    unless ARGV.length == 1
-      puts "Usage: <ruby> <file.rb> <fasta_file>"
-      exit
-      return false
+
+  # def check_arg
+  #    unless ARGV.length == 1
+  #      puts "Usage: <ruby> <file.rb> <fasta_file>"
+  #      exit
+  #      return false
+  #    end
+  #      return truev
+  #  end
+
+  def read_next_entry
+    entry = [nil,""]
+    entry[0] = @input_file.readline.chomp
+
+    tmp_pos = @input_file.pos
+    # read in sequence
+    @input_file.each do |ln|
+      if ln =~ /^>/
+        @input_file.pos = tmp_pos
+        break
+      else
+        entry[1] += ln.chomp
+        tmp_pos = @input_file.pos
+      end
     end
-      return true
+    entry
   end
-    
+
   def check_sym
-    @fasta_is_open = File.open(@input_file, "r")
-    symbol = @fasta_is_open.read(1)
+    symbol = @input_file.read(1)
     unless symbol == ">"
       puts "Usage: Not a FASTA file!"
       exit
@@ -27,19 +48,16 @@ class FastaParser
     end
       return true
   end
-  
-  def read_file
-  
-  end
-  
+
+
+
   def get_accession
-    @fasta_is_open = File.open(@input_file, "r")
-    array = @fasta_is_open.to_a
+    array = @input_file.to_a
     accession = array[0].split(/[|]/)[3]
     return true
     @fasta_is_open.close
   end
-  
+
   def get_definition
     @fasta_is_open = File.open(@input_file, "r")
     array = @fasta_is_open.to_a
@@ -48,7 +66,7 @@ class FastaParser
     return true
     @fasta_is_open.close
   end
-  
+
 end
 
 #p = FastaParser.new(input_file = ARGV[0])
--- test/test_case_file.rb
+++ test/test_case_file.rb
@@ -2,61 +2,47 @@ require 'test/unit'
 require 'fasta_parser'
 
 class TestFasta < Test::Unit::TestCase
-  
-  def test_arg
-    p = FastaParser.new(ARGV[0] = "test/test.fasta")
-    assert(p.check_arg == true)
+
+  def test_create
+    p = FastaParser.new("test/test.fasta")
+    assert_kind_of(FastaParser, p)
   end
-  
+
   def test_sym
     p = FastaParser.new("test/test.fasta")
-    assert(p.check_sym == true)
+    assert(p.check_sym)
   end
-  
-  def test_read_file
-    f = File.open("test/test.fasta", "r")
-    buffer = ''
-    index = 0
-    entry = 0
-    
-    f.each do |line|	
-      if line[0,1] == ">"
-        entry += 1
-        if buffer != ''															
-          puts buffer
-          index += 1	
-        end									
-        buffer = ''					
-        buffer << line			
-      else
-        buffer << line.sub(/\n+\z/, '')	
-      end
-    end
-    puts buffer # last entry
-    puts "Entries: #{entry}"
+
+  def test_read_entry
+    f = FastaParser.new("test/test.fasta")
+    test_entry = [">gi|329299107|ref|NM_2005745.3Acc1| Def1 zgc:65895 (zgc:65895), mRNA","AGCTCGGGGGCTCTAGCGATTTAAGGAGCGATGCGATCGAGCTGACCGTCGCG"]
+    entry = f.read_next_entry
+    assert_equal(test_entry[0], entry[0])
+    assert_equal(test_entry[1], entry[1])
   end
-  
-=begin  
+
+
+=begin
   # checks accession for first entry
   def test_accession
     p = FastaParser.new("test/test.fasta")
     assert(p.get_accession == true)
-  
+
     f = File.open("test/test.fasta","r")
     array = f.to_a
     accession = array[0].split(/[|]/)[3]
     assert_equal("NM_2005745.3Acc1", accession)
   end
-  
+
   # checks definition for first entry
-  def test_definition    
+  def test_definition
     f = File.open("test/test.fasta","r")
     array = f.to_a
     d = array[0].split(/[|]/)[4]
     definition = d.strip
     assert_equal("Def1 zgc:65895 (zgc:65895), mRNA", definition)
   end
-  
+
   # checks sequence match (NOTE: only one entry in file)
   def test_sequence
     f = File.open("test/test_entry.fasta","r")
@@ -71,5 +57,5 @@ class TestFasta < Test::Unit::TestCase
   end
 =end
 
-  
+
 end
